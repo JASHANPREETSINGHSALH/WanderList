2c66950f4e346fe884787cb519a118fc988e7882
Jashanpreet Singh Salh <jashanpreet.s0062@gmail.com>
2025-04-27 13:45:15 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlist";
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "680225f649bf8b45f759a5c9",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data was initialized");
+};
+
+initDB();
